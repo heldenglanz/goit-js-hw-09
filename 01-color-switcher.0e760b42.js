49a93359d39e6b4549dd59eb9bc6c409a636a642
heldenglanz <73900017+heldenglanz@users.noreply.github.com>
2022-06-12 14:26:31 +0000

    Deploying to gh-pages from @ heldenglanz/goit-js-hw-09@08cac8136c6601b39a699124d8d6d865d1c8a3b0 🚀

---
+++ 01-color-switcher.0e760b42.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");let n=0;t.addEventListener("click",(function(){n=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;o.style.backgroundColor=t}),1e3),t.disabled=!0})),e.addEventListener("click",(()=>{clearInterval(n),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.0e760b42.js.map
