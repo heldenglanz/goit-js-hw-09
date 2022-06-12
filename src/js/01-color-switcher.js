08cac8136c6601b39a699124d8d6d865d1c8a3b0
Heldenglanz <Heldenglanz@gmail.com>
2022-06-12 17:25:58 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startBTN = document.querySelector('button[data-start]');
+const stopBTN = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+let timer =  0;
+startBTN.addEventListener('click', colorChanger);
+stopBTN.addEventListener('click', () => { clearInterval(timer); startBTN.disabled = false; });
+const DELAY = 1000;
+
+function colorChanger() {
+    timer = setInterval(() => {
+        const color = getRandomHexColor()
+        body.style.backgroundColor = color;
+    }, DELAY);
+    startBTN.disabled=true;
+}
 
